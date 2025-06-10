dd89cfbc13c4b9af9df9cc5040546b6cd8fb1a5f
VsahilDevivers <sahil@devivers.com>
2025-06-10 15:43:09 +0530

    project setup complete with tailwind

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <h1 className='bg-black text-white'>Project Setup</h1>
+    </>
+  )
+}
+
+export default App
