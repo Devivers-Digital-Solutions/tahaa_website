c560c29fac28c97953cf172e981142fa84e25702
Sakshi <sakshibagul0502@gmail.com>
2025-06-13 11:27:25 +0530

    protfolio,contact and footer is done

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import './App.css'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import Services from './components/Services'
-import About from './components/About'
+
 import Portfolio from './components/Portfolio'
 import Contact from './components/Contact'
 import Footer from './components/Footer'
@@ -16,7 +16,7 @@ function App() {
     <Navbar />
     <Hero />
     <Services />
-    <About />
+    
     <Portfolio/>
     <Contact/>
     <Footer/>
